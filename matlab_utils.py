e4f26d431aa77fc763cd11c383cd95ecc45b0e96
Sulabh Shrestha <sulabh.shr@gmail.com>
2020-01-28 10:34:04 -0500

    Fix bug that returned alias of tR params. Add world position functions.

--- matlab_utils.py
+++ matlab_utils.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 from scipy.io import loadmat
 
 
@@ -26,9 +27,44 @@ def get_tR(image_name, image_struct):
         row_img_name = row[0][0]
 
         if row_img_name == image_name:
-            t = row[1]
-            R = row[2]
+            t = row[1].copy()
+            R = row[2].copy()
             
             return t, R
 
     return None 
+
+
+def get_world_pos(image_name, image_struct):
+
+    for idx, row in enumerate(image_struct):
+        row_img_name = row[0][0]
+
+        if row_img_name == image_name:
+            return row[3][:, 0].copy()
+
+    return None
+
+
+def get_all_world_pos(image_struct):
+    world_pos_list = []
+
+    for row in image_struct:
+        world_pos_list.append(row[3][:, 0].copy())
+
+    return np.unique(world_pos_list, axis=0)
+
+
+def get_image_from_nodes(nodes, image_struct):
+
+    images = []
+    nodes = nodes.tolist()
+
+    for row in image_struct:
+        world_pos = list(row[3][:, 0][[0, 2]])
+
+        if world_pos in nodes:
+            row_img_name = row[0][0]
+            images.append(row_img_name)
+
+    return images
