eb7518a1102369a77ef69610f2b90f1647fd6ffb
Sulabh Shrestha <sulabh.shr@gmail.com>
2020-02-18 15:15:10 -0500

    Add handling of per scene camera params file structure

--- camera_param_utils.py
+++ camera_param_utils.py
@@ -1,27 +1,35 @@
 import os
 from .defaults import camera
 
-def load_camera_params(dataset_root, scene):
-	CAMERA_PARAMS_FOLDER = 'camera_params'
-	CAMERA_PARAMS_FILENAME = 'cameras.txt'
 
-	scene_params_path = os.path.join(dataset_root, CAMERA_PARAMS_FOLDER, 
-		scene, CAMERA_PARAMS_FILENAME)
+def load_camera_params(dataset_root, scene, return_default=False):
+    CAMERA_PARAMS_FOLDER = 'camera_params'
+    CAMERA_PARAMS_FILENAME = 'cameras.txt'
 
-	if os.path.isfile(scene_params_path):
-		with open(scene_params_path, 'r') as f:
-			file_contents = f.read()
-		params_line = file_contents.split('\n')[3]
-		params_split = params_line.split(' ')
+    camera_params_path = os.path.join(dataset_root, CAMERA_PARAMS_FOLDER)
 
-		return {
-			'fx': float(params_split[4]),
-			'fy': float(params_split[5]),
-			'cx': float(params_split[6]),
-			'cy': float(params_split[7])
-		}
-	print(f'Camera parameters not found for **{scene}** in \n\t {scene_params_path}.'
-		'\nReturning default values.')
+    if os.path.isdir(camera_params_path):
+        scene_params_path = os.path.join(camera_params_path, scene)
+    else:
+        scene_params_path = os.path.join(dataset_root, scene)
 
-	return camera['intrinsics']
+    scene_params_path = os.path.join(scene_params_path, CAMERA_PARAMS_FILENAME)
 
+    if os.path.isfile(scene_params_path):
+        with open(scene_params_path, 'r') as f:
+            file_contents = f.read()
+        params_line = file_contents.split('\n')[3]
+        params_split = params_line.split(' ')
+
+        return {
+            'fx': float(params_split[4]),
+            'fy': float(params_split[5]),
+            'cx': float(params_split[6]),
+            'cy': float(params_split[7])
+        }
+    elif return_default:
+        print(f'Camera parameters not found for **{scene}** in \n\t {scene_params_path}.'
+              '\nReturning default values.')
+        return camera['intrinsics']
+    else:
+        raise FileNotFoundError(f'Path {scene_params_path} does not exists!')
