b4583e90ce8f351643a5a458943d2288fb32094e
Sulabh Shrestha <sulabh.shr@gmail.com>
2020-09-14 19:49:49 -0400

    Add navigation option for category specific boxes

--- activevision/viz_nav.py
+++ activevision/viz_nav.py
@@ -3,13 +3,16 @@ from PIL import Image
 import matplotlib.pyplot as plt
 
 from activevision.defaults import AVD_DATASET
-from activevision.annotations import AVDAnnotations
+from activevision.annotations import AVDAnnotations, AVDCategoryAnns
 from activevision.utils.plot_utils import bboxplot_in_img
 
 
-def visualize_boxes_and_move(scene, start_idx=0):
+def visualize_boxes_and_move(scene, box_type='instance', start_idx=0):
     data_root = AVD_DATASET
-    scene_ann = AVDAnnotations(data_root=data_root)
+    if box_type == 'instance':
+        scene_ann = AVDAnnotations(data_root=data_root)
+    else:
+        scene_ann = AVDCategoryAnns(data_root=data_root)
     scene_ann.load_annotations(scenes=[scene])
 
     input_map = {
@@ -46,9 +49,20 @@ def visualize_boxes_and_move(scene, start_idx=0):
 
         img_path = scene_ann.img_name2path(scene=scene, name=img_name)
         img = Image.open(img_path)
-        boxes_dict = scene_ann.get_image_boxes(scene, img_name)
+        boxes_dict = scene_ann.get_image_boxes(scene, img_name, box_type=box_type)
+        bboxes = []
+        labels = []
+
+        if box_type == 'instance' or box_type == 'both':
+            bboxes += list(boxes_dict['instance_boxes'])
+            labels += boxes_dict['instance_names']
+        if box_type == 'category':
+            bboxes += list(boxes_dict['category_boxes'])
+            labels += boxes_dict['category_names']
+
         plt.cla()
-        fig, ax = bboxplot_in_img(img, bboxes=boxes_dict['boxes'], labels=boxes_dict['instance_names'],
+        fig, ax = bboxplot_in_img(img, bboxes=bboxes,
+                                  labels=labels,
                                   fig=fig, ax=ax, return_fig=True)
         ax.set_title(img_name)
         plt.draw()
@@ -61,8 +75,11 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--scene', '-s', type=str, required=True,
                         help='Name of scene to visualize.')
+    parser.add_argument('--type', type=str, required=True, choices=['instance', 'category', 'both'],
+                        help='Type of bounding boxes to display')
     parser.add_argument('--idx', type=int, required=False, default=0,
                         help='Index to start visualizations from.')
+
     args = parser.parse_args()
 
-    visualize_boxes_and_move(scene=args.scene, start_idx=args.idx)
+    visualize_boxes_and_move(scene=args.scene, box_type=args.type, start_idx=args.idx)
